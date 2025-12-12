42862d78a498899eca248b2f5cffc093017485c9
shriarthi <97339258+shriarthi@users.noreply.github.com>
2025-12-12 14:54:31 +0100

    Update setup_elk.sh

--- scripts/setup_elk.sh
+++ scripts/setup_elk.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Simple Docker ELK stack for testing
+
+docker network create elk-net
+
+docker run -d --name elasticsearch --net elk-net -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.11.0
+
+docker run -d --name kibana --net elk-net -p 5601:5601 docker.elastic.co/kibana/kibana:8.11.0
+
+docker run -d --name logstash --net elk-net -v $(pwd)/logstash:/usr/share/logstash/pipeline -p 5044:5044 docker.elastic.co/logstash/logstash:8.11.0
 
